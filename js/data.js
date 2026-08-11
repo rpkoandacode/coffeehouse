a4ba8a4b277225ff6e663163ccf867c1ecb29ec9
Richard Pratama Koanda <rpkoanda@gmail.com>
2026-08-11 12:37:38 +0800

    image added and data.js created

---
+++ js/data.js
@@ -0,0 +1,69 @@
+const products = [
+  // Coffee
+  {
+    id: 1,
+    name: "Espresso",
+    category: "Coffee",
+    price: 25000,
+    image: "images/coffee/espresso.jpg",
+    description: "A bold and concentrated shot of espresso."
+  },
+  {
+    id: 2,
+    name: "Americano",
+    category: "Coffee",
+    price: 28000,
+    image: "images/coffee/americano.jpg",
+    description: "Smooth espresso diluted with hot water."
+  },
+  {
+    id: 3,
+    name: "Cappuccino",
+    category: "Coffee",
+    price: 35000,
+    image: "images/coffee/cappuccino.jpg",
+    description: "Rich espresso with steamed milk and foam."
+  },
+  {
+    id: 4,
+    name: "Cafe Latte",
+    category: "Coffee",
+    price: 38000,
+    image: "images/coffee/latte.jpg",
+    description: "Creamy milk with smooth espresso."
+  },
+  {
+    id: 5,
+    name: "Flat White",
+    category: "Coffee",
+    price: 38000,
+    image: "images/coffee/flat-white.jpg",
+    description: "Velvety steamed milk with double espresso."
+  },
+  {
+    id: 6,
+    name: "Mocha",
+    category: "Coffee",
+    price: 42000,
+    image: "images/coffee/mocha.jpg",
+    description: "Chocolate and espresso combined into one drink."
+  },
+
+  // Non Coffee
+  {
+    id: 7,
+    name: "Matcha Latte",
+    category: "Non Coffee",
+    price: 40000,
+    image: "images/non-coffee/matcha.jpg",
+    description: "Premium Japanese matcha with fresh milk."
+  },
+  {
+    id: 8,
+    name: "Chocolate",
+    category: "Non Coffee",
+    price: 38000,
+    image: "images/non-coffee/chocolate.jpg",
+    description: "Rich chocolate blended with creamy milk."
+  }
+];
